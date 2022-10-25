02e25f7942e6aabb0144626b588ecbb1bb4007fb
Abu Siam <siamcse01@gmail.com>
2022-10-25 17:54:00 +0600

    courses data edited

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/',(req,res)=>{
     res.send('hello world');
+})
+
+app.get('/courses',(req,res)=>{
+    res.send(courses);
 })
 
 app.listen(port, ()=>{
